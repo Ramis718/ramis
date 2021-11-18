4b7a1e2dca892835bf7e4e7aa8b389add408123c
Ramis718 <93920082+Ramis718@users.noreply.github.com>
2021-11-18 21:47:38 +0600

    Add files via upload

--- Ramis_homework 6.py
+++ Ramis_homework 6.py
@@ -1,5 +1,9 @@
 class Algorithm:
-   def sum_number(self):
+    def __init__(self, num_1, result):
+        self.num_1 = num_1
+        self.result = result
+
+    def sum_number(self):
        numbers = [2, 7, 11, 15]
        desired_sum = 9
        for i in numbers:
@@ -8,9 +12,11 @@ class Algorithm:
                    # return print(i, a)
                     print([numbers.index(i), numbers.index(a)])
 
+    def __str__(self):
+        return f'Name: {self.num_1}\n' \
+               f'Result: {self.result}'
 
 
-
-
-algorithm = Algorithm()
+algorithm = Algorithm(num_1= '2, 7', result='9')
+print(algorithm)
 algorithm.sum_number()
